8766d76d8875313ea5a4dba742dea371c8cc806d
Calterat <Calterat@gmail.com>
2020-10-14 01:05:59 -0500

    added attacks on each side and a checkhealth function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-let playerName = window.prompt("What is your robot's name?");
+let playerName = prompt("What is your robot's name?");
 let playerHealth = 100;
 let playerAttack = 10;
 
@@ -11,7 +11,15 @@ let enemyAttack = 12;
 const fight = () => {
 
     // Alert users that they are starting the round
-    window.alert("Welcome to Robot Gladiators");
+    alert("Welcome to Robot Gladiators");
+
+    const checkhealth = (name, health) => {
+        if (health <= 0) {
+            return name + " has died!";
+        } else {
+            return name + " still has " + health + " health left";
+        }
+    }
 
     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
     enemyHealth -= playerAttack;
@@ -19,12 +27,19 @@ const fight = () => {
     // Log a resulting message to the console so we know that it worked.
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + "now has " + enemyHealth + " health remaining.");
 
+     // check enemy's health
+    alert(checkhealth(enemyName, enemyHealth));
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
     playerHealth -= enemyAttack;
 
     // Log a resulting message to the console so we know that it worked.
     console.log(enemyName + " attacked " + playerName + ". " + playerName + "now has " + playerHealth + " health remaining.");
 
+    // check player's health
+    alert(checkhealth(playerName, playerHealth));
+   
+
 }
 
 fight();
+
