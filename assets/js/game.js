873aafc66633f3ccc2ca0b2c0ff65460aee7bf40
Calterat <Calterat@gmail.com>
2020-10-15 01:20:36 -0500

    added objects

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,6 @@
-let playerName = prompt("What is your robot's name?");
-let playerHealth = 100;
-let playerAttack = 10;
-let playerMoney = 10;
+const fight = (enemy) => {
 
-// You can also log multiple values at once like this: console.log(playerName, playerAttack, playerHealth);
-
-const enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-let enemyHealth = 50;
-let enemyAttack = 12;
-
-
-// Game States
-// "WIN" - Player robot has defeated all enemy-robots
-//  * Fight all enemy-robots
-//  * Defeat each enemy-robot
-// "LOSE" - Player robot's health is zero or less
-
-
-
-const fight = (enemyName) => {
-
-    while(enemyHealth > 0 && playerHealth > 0) {
+    while(enemy.health > 0 && playerInfo.health > 0) {
 
         let promptFight = prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -29,10 +9,10 @@ const fight = (enemyName) => {
             let confirmSkip = confirm("Are you sure you'd like to quit?");
             // if yes (true), leave fight
             if (confirmSkip) {
-                alert(playerName + " has decided to skip this fight. Goodbye!");
+                alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
                 //subtract monies
-                playerMoney = Math.max(0, playerMoney - 10);
-                console.log("playerMoney", playerMoney);
+                playerInfo.money = Math.max(0, playerInfo.money - 10);
+                console.log("playerInfo.money", playerInfo.money);
                 break;
             }
         }
@@ -47,30 +27,30 @@ const fight = (enemyName) => {
                 }
             }
 
-            //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
-            let damage = randomNumber(playerAttack - 3, playerAttack);
-            enemyHealth = Math.max(0, enemyHealth - damage);
+            //Subtract the value of 'playerInfo.attack' from the value of 'enemy.health' and use that result to update the value in the 'enemy.health' variable.
+            let damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+            enemy.health = Math.max(0, enemy.health - damage);
 
             // Log a resulting message to the console so we know that it worked.
-            console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+            console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
 
             // check enemy's health
-            alert(checkhealth(enemyName, enemyHealth));
-            if (enemyHealth <= 0) {
+            alert(checkhealth(enemy.name, enemy.health));
+            if (enemy.health <= 0) {
                 break;
              }
         
-            // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-            damage = randomNumber(enemyAttack - 3, enemyAttack);
-            playerHealth = Math.max(0, playerHealth - damage);
+            // Subtract the value of 'enemy.attack' from the value of 'playerInfo.health' and use that result to update the value in the 'playerInfo.health' variable.
+            damage = randomNumber(enemy.attack - 3, enemy.attack);
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
 
             // Log a resulting message to the console so we know that it worked.
-            console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+            console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
 
             // check player's health
-            alert(checkhealth(playerName, playerHealth));
+            alert(checkhealth(playerInfo.name, playerInfo.health));
             // kicks you out of the loop/fight if you are dead
-            if (playerHealth <= 0) {
+            if (playerInfo.health <= 0) {
                 break;
             }
         }        
@@ -81,18 +61,16 @@ const fight = (enemyName) => {
 
 const startGame = () => {
     // reset player stats
-    playerHealth = 100;
-    playerAttack = 10;
-    playerMoney = 10;
-    for ( i = 0; i < enemyNames.length; i++) {
-        if (playerHealth >0) {
+    playerInfo.reset();
+    for ( i = 0; i < enemyInfo.length; i++) {
+        if (playerInfo.health >0) {
             alert("Welcome to Robot Gladiators! Round " + (i+1));
-            let pickedEnemyName = enemyNames[i];
+            let pickedEnemyObj = enemyInfo[i];
             // min max of 40-60hp for enemy @ random
-            enemyHealth = randomNumber(40, 60);
-            fight(pickedEnemyName);
+            pickedEnemyObj.health = randomNumber(40, 60);
+            fight(pickedEnemyObj);
             // if we're not at the last enemy in the array
-            if (i < enemyNames.length - 1 && playerHealth > 0) {
+            if (i < enemyInfo.length - 1 && playerInfo.health > 0) {
                 // ask if player want to use the sore before next round
                 let storeConfirm = confirm("The fight is over, visit the store before the next round?");
                 // if yes, take them to the store() function
@@ -112,8 +90,8 @@ const startGame = () => {
 
 const endGame = () => {
     // if player is still alive, player wins!
-    if (playerHealth > 0) {
-        alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    if (playerInfo.health > 0) {
+        alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
     } else {
         alert("You've lost your robot in battle.");
     }
@@ -136,27 +114,11 @@ const shop = () => {
     switch (shopOptionPrompt) {
         case "REFILL":
         case "refill":
-            if (playerMoney >= 7) {
-                alert("Refilling player's health by 20 for 7 dollars.");
-
-                // increase health and decrease money
-                playerHealth += 20;
-                playerMoney -= 7;
-            } else {
-                alert("You don't have enough money!");
-            }
+            playerInfo.refillHealth();
             break;
         case "UPGRADE":
         case "upgrade":
-            if (playerMoney >= 7) {
-                alert("Upgrading player's attack by 6 for 7 dollars.");
-
-                // increase health and decrease money
-                playerAttack += 6;
-                playerMoney -= 7;
-            } else {
-                alert("You don't have enough money!");
-            }
+            playerInfo.upgradeAttack();
             break;
         case "LEAVE":
         case "leave":
@@ -178,6 +140,50 @@ const randomNumber = (min, max) => {
     return value; 
 }
 
+const playerInfo = {
+    name: prompt("What is your robot's name?"),
+    health: 100,
+    attack: 10,
+    money: 10,
+    reset: function() {
+        this.health = 100;
+        this.money = 10;
+        this.attack = 10;
+    },
+    refillHealth: function() {
+        if (this.money >= 7) {
+            this.health += 20;
+            this.money -=7;
+        } else {
+            alert("You don't have enough money!");
+        }
+    },
+    upgradeAttack: function() {
+        if (this.money >= 7) {
+            this.attack += 6;
+            this.money -=7;
+        } else {
+            alert("You don't have enough money!");
+        }
+    }
+}
+
+// You can also log multiple values at once like this: console.log(playerInfo.name, playerInfo.attack, playerInfo.health);
+
+const enemyInfo = [
+    {
+        name: "Roborto",
+        attack: randomNumber(10, 14)
+    },
+    {
+        name: "Amy Android",
+        attack: randomNumber(10, 14)
+    },
+    {
+        name: "Robo Trumble",
+        attack: randomNumber(10, 14)
+    }
+];
 
 
 
